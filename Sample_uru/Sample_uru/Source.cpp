a1068726e41436f71105be9cdbb7f251ec3b1d4b
名取 伸梧 <mikomiko_spark_jaooo@yahoo.co.jp>
2016-05-23 19:33:10 +0900

    西暦を入力すると，うるう年かどうかわかります．

---
+++ Sample_uru/Sample_uru/Source.cpp
@@ -0,0 +1,23 @@
+//西暦を入力し，閏年かどうか判定する．
+#include <stdio.h>
+
+int main(void)
+{
+	int AD;
+
+	printf("西暦の年数=>");
+	scanf_s("%d", &AD);
+
+	if (
+		(AD % 4 == 0 && AD % 100 && AD % 400 == 0) 
+		|| (AD % 4 == 0 && !(AD % 100 == 0) && AD % 400 == 0)
+		|| (AD % 4 == 0 && !(AD % 100 == 0) && !(AD % 400 == 0))
+		)
+	{
+		printf("%d年は閏年です．\n", AD);
+	}
+	else {
+		printf("%d年は閏年ではありません．\n", AD);
+	}
+	return 0;
+}
