d9efd2d6ff5d0920e9bbd2794764f37902b5733e
名取 伸梧 <mikomiko_spark_jaooo@yahoo.co.jp>
2016-05-30 15:07:35 +0900

    文字を判別して，アルファベット大文字は小文字，小文字は大文字，アルファベット以外ははじくプログラムです．

---
+++ Sample_aA/Sample_aA/Source.cpp
@@ -0,0 +1,24 @@
+//文字を判別して小文字を大文字，大文字を小文字，それ以外をはじく
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+
+	printf("文字を入力=>");
+	a = getchar();
+
+	if ('\x41'<= a && a <= '\x5a') {
+		printf("文字「%c」は大文字です．", a);
+		printf("小文字は「%c」です\n", a + 32);
+	}
+	else if ('\x61' <= a && a <= '\x7a') {
+		printf("文字「%c」は小文字です．", a);
+		printf("大文字は「%c」です\n", a - 32);
+	}
+	else {
+		printf("アルファベット以外です\n");
+	}
+
+	return 0;
+}
